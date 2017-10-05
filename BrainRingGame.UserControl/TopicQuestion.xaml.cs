0ce6f7fa6b65f261d7f19030fecc42db3527cedd
Zhenya1256 <vovchok.zhenya@gmail.com>
2017-10-06 00:49:35 +0300

    AddTimerMVVM

---
+++ BrainRingGame.UserControl/TopicQuestion.xaml.cs
@@ -0,0 +1,115 @@
+﻿using BrainRingGame.BL.Impl.ImageProcessor;
+using System;
+using System.Collections.Generic;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BrainRingGame.UserControl
+{
+    /// <summary>
+    /// Interaction logic for TopicQuestion.xaml
+    /// </summary>
+    public partial class TopicQuestion : System.Windows.Controls.UserControl
+    {
+        private AddImageControl _imageControl;
+        private ChangeImage _changeImage;
+        public Action<Key> KeyDown;
+        private int _index;
+
+        public TopicQuestion()
+        {
+            InitializeComponent();
+            _imageControl = new AddImageControl();
+            _changeImage = new ChangeImage();
+            InitialImage();
+            InititialList();
+        }
+
+        private void InitialImage()
+        {
+            //
+            System.Drawing.Image image = BrainRingGame.Ui.Wpf.Recourses.Components
+                .Properties.Resources.QuestionMp3;
+            //
+
+            MemoryStream streamImage = new MemoryStream();
+            image.Save(streamImage, ImageFormat.Png);
+            int width = System.Windows.Forms.Screen.
+                PrimaryScreen.WorkingArea.Width;
+            int height = System.Windows.Forms.Screen.
+                PrimaryScreen.WorkingArea.Height;
+            double subHeight = height / 1.65;
+            height = (int)subHeight;
+            streamImage = _changeImage.ChangeSize(streamImage, width, height);
+
+            QuestionImage.Source = _imageControl.AddControlImage(streamImage);
+        }
+
+        private void InititialList()
+        {
+            _index = 0;
+
+            if (listViewCommand.Items.Count > 0)
+            {
+                listViewCommand.SelectedIndex = _index;
+            }
+        }
+
+        public void HandleKeyPress(Key key)
+        {
+            if (KeyDown != null)
+            {
+                KeyDown.Invoke(key);
+            }
+            switch (key)
+            {
+                case Key.Space:
+                    listViewCommand.Visibility = Visibility.Visible;
+                    break;
+                case Key.OemMinus:
+                    listViewCommand.Visibility = Visibility.Hidden;
+                    break;
+                case Key.Up:
+                    UpKey();
+                    break;
+                case Key.Down:
+                    DownKey();
+                    break;
+            }
+        }
+
+        private void UpKey()
+        {
+            if (_index != 0)
+            {
+                _index--;
+            }
+            listViewCommand.SelectedIndex = _index;
+        }
+        private void DownKey()
+        {
+            if (_index + 1 < listViewCommand.Items.Count)
+            {
+                _index++;
+            }
+            listViewCommand.SelectedIndex = _index;
+
+        }
+
+        private void listBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+        }
+    }
+}
---
+++ BrainRingGame.ViewModel/ViewModalForUserControls/VMTopicQuestion.cs
@@ -0,0 +1,194 @@
+﻿using BrainRingGame.Entity.Impl.GameEntity;
+using BrainRingGame.Ui.Wpf.Common.Recourses.ViewModel.Abstaract;
+using BrainRingGame.Ui.Wpf.Common.Recourses.ViewModel.Base;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows.Threading;
+
+namespace BrainRingGame.ViewModel.ViewModalForUserControls
+{
+  public  class VMTopicQuestion : ViewModelBase
+    {
+        private IMainWindowsCodeBehind _codeBehind;
+        private DispatcherTimer _disTimer;
+        private double _second;
+        private string _timerValue;
+        private int _size;
+        private ObservableCollection<TeamGroup> _group;
+
+        public VMTopicQuestion(IMainWindowsCodeBehind codeBehind)
+        {
+            _codeBehind = codeBehind;
+            _size=116;
+            _second = 16.1;
+            // double.Parse(ConfigurationManager.AppSettings["Second"]);
+            Timer();
+        }
+
+        #region Timer
+
+        public int SizeText
+        {
+            get { return _size; }
+            set
+            {
+                _size = value;
+                OnPropertyChanged("SizeText");
+            }
+        }
+
+        public string TimerValue
+        {
+            get { return _timerValue; }
+            set
+            {
+                _timerValue = value;
+                OnPropertyChanged("TimerValue");
+            }
+        }
+
+        private void Timer()
+        {
+            _disTimer = new DispatcherTimer();
+            _disTimer.Interval = TimeSpan.FromMilliseconds(100); 
+            _disTimer.Tick += dtTicker;
+            _disTimer.Start();
+        }
+
+        private void dtTicker(object sender, EventArgs e)
+        {
+            if (_second > 0.1)
+            {
+                if (_second <= 4)
+                {
+                    FourSecond();
+                }
+                _second = _second - 0.1;
+
+                string strSecond = _second.ToString();
+
+                if (strSecond.Contains(","))
+                {
+                    int point = strSecond.IndexOf(",");
+                    if (strSecond.Length > point + 2)
+                    {
+                        strSecond = strSecond.Remove(point + 2, strSecond.Length - 3);
+                    }
+                }
+                else
+                {
+                    strSecond = strSecond + ",0";
+
+                }
+
+                if (_second < 10)
+                {
+                    TimerValue = "0" + strSecond.Replace(",", ":");
+                }
+                else
+                {
+                    TimerValue = strSecond.Replace(",", ":");
+                }
+            }
+            if (_second <= 0.1 || _second == 4.0)
+            {
+                _disTimer.Stop();
+                File.Delete("TempPLayer.mp3");
+                _codeBehind.LoadView(Entity.Abstract.Enums.ViewType.Thems);
+            }
+
+        }
+
+        private void FourSecond()
+        {
+            string str = "1";
+            
+            if (_second.ToString().Contains(","))
+            {
+                str = _second.ToString().Split(',')[1];
+            }
+            char ch = str.ToCharArray()[0];
+
+            int sec = int.Parse(ch.ToString());
+
+            if (sec % 4 == 0)
+            {
+                SizeText = 116;
+            }
+            else if (sec % 3 == 0)
+            {
+                SizeText = 122;
+            }
+            else if (sec % 5 == 0)
+            {
+                SizeText = 116;// 130;
+            }
+            else if (sec % 2 == 0)
+            {
+                SizeText = 122;// 125;
+            }
+        }
+
+        #endregion
+
+        #region EventKeys
+
+        public void HandleKeyPress(Key key)
+        {
+            switch (key)
+            {
+                case Key.Space:
+                    Space();
+                    break;
+
+                case Key.OemPlus:
+                    OemPlus();
+                    break;
+                case Key.OemMinus:
+                    OemMinus();
+                    break;
+  
+                case Key.Return:
+                    break;
+            }
+        }
+
+        private void Space()
+        {
+            _disTimer.Stop();
+        }
+
+        private void OemPlus()
+        {
+            _codeBehind.LoadView(Entity.Abstract.Enums.ViewType.Thems);
+        }
+
+        private void OemMinus()
+        {
+            _disTimer.Start();
+        }
+
+        #endregion
+   
+        public ObservableCollection<TeamGroup> ListGroups
+        {
+            get
+            {
+                if (_group == null)
+                {
+                    _group = new ObservableCollection<TeamGroup>();
+                }
+
+                return _group;
+            }
+        }
+
+    }
+}
